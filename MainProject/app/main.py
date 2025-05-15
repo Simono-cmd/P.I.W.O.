d5a72825a2ea7c47b9a159f7d3936919f271b661
s30635 <s30635@pjwstk.edu.pl>
2025-05-15 21:06:39 +0200

    add: new repositories layer

--- MainProject/app/main.py
+++ MainProject/app/main.py
@@ -1,5 +1,3 @@
 # TODO na 15.05.2025
-# - napisanie do cwiczeniowca, czy mozemy zrobic projekt z uwzglednieniem wielu przedmiotow
-# - jesli tak, to stworzenie nowej tabeli Zagrozenia, ktora mowi, czy student jest zagrozony z danego przedmiotu
 # - dodanie tej tabeli do kodu
 # - poprawienie logiki (dostosowanie metod i modeli)
