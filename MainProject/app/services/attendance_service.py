8a7a62303026822699e4834bb4a26d27513e77aa
Simono-cmd <trauth.szymon@gmail.com>
2025-05-15 21:04:36 +0200

    service update

--- MainProject/app/services/attendance_service.py
+++ MainProject/app/services/attendance_service.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 from app.database.database import SessionLocal
 from app.models.attendance import Attendance
 from sqlalchemy.exc import NoResultFound
--- MainProject/app/services/student_service.py
+++ MainProject/app/services/student_service.py
@@ -1,6 +1,7 @@
 from sqlalchemy.exc import NoResultFound
 from app.database.database import SessionLocal
 from app.models.attendance import Attendance
+from app.models.failure import Failure
 from app.models.grade import Grade
 from app.models.student import Student
 
@@ -68,54 +69,39 @@ class StudentService:
             session.close()
 
     @staticmethod
-    def evaluate_student_risk(student_id: int):
+    def get_student_attendances_from_subject(student_id: int, subject_id: int):
         session = SessionLocal()
         try:
-            student = session.query(Student).get(student_id)
-            if not student:
-                raise ValueError("Student not found")
-
-            #calculate absences
-            absences = session.query(Attendance).filter(
-                Attendance.student_id == student_id,
-                Attendance.status == "absent"
-            ).count()
-
-            #calculate being late
-            total_attendances = session.query(Attendance).filter(
-                Attendance.student_id == student_id
-            ).count()
-            late_attendances = session.query(Attendance).filter(
-                Attendance.student_id == student_id,
-                Attendance.status == "late"
-            ).count()
-
-            #calculate avg
-            grades = session.query(Grade.worth).filter(Grade.student_id == student_id).all()
-            total_grades = len(grades)
-            average = sum([g[0] for g in grades]) / total_grades if total_grades > 0 else 0
-
-            warnings = []
-
-            if absences > 2:
-                warnings.append("Więcej niż 2 nieobecności")
-
-            if total_attendances > 0 and late_attendances > (total_attendances / 2):
-                warnings.append("Spóźnienia na więcej niż połowę lekcji")
-
-            if average < 3:
-                warnings.append("Średnia ocen poniżej 3")
-
-            student.atRisk = len(warnings) > 0
-            session.commit()
-
-            return warnings
-
-        except Exception as e:
-            session.rollback()
-            raise e
+            return session.query(Attendance).filter_by(
+                student_id=student_id,
+                subject_id=subject_id
+            ).all()
         finally:
             session.close()
+
+    @staticmethod
+    def get_student_grades_from_subject(student_id: int, subject_id: int):
+        session = SessionLocal()
+        try:
+            return session.query(Grade).filter_by(
+                student_id=student_id,
+                subject_id=subject_id
+            ).all()
+        finally:
+            session.close()
+
+    @staticmethod
+    def is_student_at_risk(student_id: int, subject_id: int) -> bool:
+        session = SessionLocal()
+        try:
+            failure = (session.query(Failure).filter_by(
+                student_id=student_id,
+                subject_id=subject_id)
+            .first())
+            return failure is not None
+        finally:
+            session.close()
+
 
 
 
