4a4dca10bbec4d3e787162fc5497789b795f5649
Simono-cmd <trauth.szymon@gmail.com>
2025-05-22 01:15:26 +0200

    todo

--- MainProject/app/main.py
+++ MainProject/app/main.py
@@ -14,7 +14,8 @@ try:
     # GradeService.add_grade(session=session, student_id=1, subject_id=2, form="test", worth=2)
     # GradeService.add_grade(session=session, student_id=1, subject_id=1, form="short test", worth=4)
 
-    # StudentService.generate_statistics_for_student(session, 2)
+    StudentService.generate_statistics_for_student(session, 2)
+    StudentService.generate_statistics_for_everyone(session)
     # StudentService.generate_grades_report(session, 1)
     # StudentService.generate_attendance_report(session, 1)
     session.close()
@@ -25,3 +26,9 @@ finally:
         app = MainWindow()
         app.run()
 
+# TODO
+# 1. przycisk statystyk ma zwracac statystyki dla konkretnego przedmiotu, a nie dla kazdego przedmiotu
+# 2. przycisku statystyk i reportu maja zwracac staty dla konkretnego studenta
+# 3. dokonczyc grades i attendances buttony
+# 4. ogarnac wyjatki
+# 5. generowanie przykladowych danych
