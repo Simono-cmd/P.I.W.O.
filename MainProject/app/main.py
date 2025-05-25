bcf5251a3e121eaeaaf78fdc895f7c392f45ed52
Simono-cmd <trauth.szymon@gmail.com>
2025-05-25 21:34:51 +0200

    final

--- MainProject/app/main.py
+++ MainProject/app/main.py
@@ -2,23 +2,37 @@ from MainProject.app.database.database import SessionLocal
 from MainProject.app.gui.main_window import MainWindow
 
 session = SessionLocal()
+
+print(
+    """
+    
+                                        ╔═══════════════════════════════════╗
+                                        ║         PROJECT \033[93mP.I.W.O\033[0m           ║
+                                        ╠═══════════════════════════════════╣
+                                        ║   Authors : Szymon Trauth         ║
+                                        ║             Michał Maksymiuk      ║
+                                        ║                                   ║
+                                        ║            2025 PJATK             ║
+                                        ╚═══════════════════════════════════╝
+                                        
+    """
+)
+
 try:
+    print("\033[92mrunning")
     app = MainWindow(session)
     app.run()
     session.close()
 finally:
     session.commit()
     session.close()
+    print("\033[91mshut down")
 
 
-# TODO
-# 1. podpiac raporty
-# 2. ogarnac wyjatki
 
 
-# dotenv
-# sqlalchemy
-# psycopg2
-# customtkinkter
-# openpyxl
-# matplotlib
+
+
+
+
+
